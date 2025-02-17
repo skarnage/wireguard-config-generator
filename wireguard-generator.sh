60781b467f699d6866faf1bf236c32d62f974589
Lou Simard <lou.simard@collegealma.ca>
2025-02-17 13:53:08 -0500

    First commit

--- wireguard-generator.sh
+++ wireguard-generator.sh
@@ -4,10 +4,10 @@
 wg_interface=("wireguard" "" "48450")
 
 # Wireguard interface, Wireguard public key, name, ip, dns, comment, endpoint:port
-wg_peer0=("wireguard" "2TYAnfYAgDEQhcukVzQPOJmLJc6l5wl7JjJ+8wVpLUw=" "Lou" "192.168.216.10" "192.168.2.1" "" "107.159.60.200:48450")
-wg_peer1=("wireguard" "2TYAnfYAgDEQhcukVzQPOJmLJc6l5wl7JjJ+8wVpLUw=" "Claudine" "192.168.216.11" "192.168.2.1" "" "107.159.60.200:48450")
-wg_peer2=("wireguard" "2TYAnfYAgDEQhcukVzQPOJmLJc6l5wl7JjJ+8wVpLUw=" "Loxanh" "192.168.216.12" "192.168.2.1" "" "107.159.60.200:48450")
-wg_peer3=("wireguard" "2TYAnfYAgDEQhcukVzQPOJmLJc6l5wl7JjJ+8wVpLUw=" "Ezekiel" "192.168.216.13" "192.168.2.1" "" "107.159.60.200:48450")
+wg_peer0=("wireguard" "2TYAnfYAgDEQhcukVzQPOJmLJc6l5wl7JjJ+8wVpLUw=" "XXX" "192.168.216.10" "192.168.2.1" "" "example.com:48450")
+wg_peer1=("wireguard" "2TYAnfYAgDEQhcukVzQPOJmLJc6l5wl7JjJ+8wVpLUw=" "YYY" "192.168.216.11" "192.168.2.1" "" "example.com:48450")
+wg_peer2=("wireguard" "2TYAnfYAgDEQhcukVzQPOJmLJc6l5wl7JjJ+8wVpLUw=" "ZZZ" "192.168.216.12" "192.168.2.1" "" "example.com:48450")
+wg_peer3=("wireguard" "2TYAnfYAgDEQhcukVzQPOJmLJc6l5wl7JjJ+8wVpLUw=" "AAA" "192.168.216.13" "192.168.2.1" "" "example.com:48450")
 
 # WG Network, WG port, LAN network
 firewall=("192.168.216.0/24" "48450" "192.168.2.0/24")
